0e62d8fea166cd856085220192641219e80fbe5c
Iuri Carrasqueiro <iuri.carrasqueiro@gmail.com>
2025-06-02 16:43:36 +0100

    Refactor socket connection to use environment variables for daemon IP and token

--- package/ws/__init__.py
+++ package/ws/__init__.py
@@ -14,6 +14,16 @@ port = os.getenv("PROXMOX_PORT")
 user = os.getenv("PROXMOX_USER")
 password = os.getenv("PROXMOX_PASSWORD")
 
+daemonip = os.getenv("DAEMON_IP")
+daemontoken = os.getenv("DAEMON_TOKEN")
+
+
+if not ip or not port or not user or not password:
+    raise ValueError("Proxmox connection details are not set in the environment variables.")
+
+if not daemonip or not daemontoken:
+    raise ValueError("Daemon connection details are not set in the environment variables.")
+
 proxmox = ProxmoxAPI(ip + ":"+ port, user=user, password=password, verify_ssl=False, timeout=30)
 presources = Resources()
 
@@ -22,7 +32,7 @@ threading.Thread(target=APIThread, args=(proxmox, presources)).start()
 
 
 try:
-   sio.connect('http://localhost:5000', auth={'ip': "192.168.22.14", 'token': 'DitkYWBEv8cnAyCs'})
+   sio.connect('http://localhost:5000', auth={'ip': daemonip, 'token': daemontoken})
 except socketio.exceptions.ConnectionError as e:
     print(f"Connection failed: {e}")
 
